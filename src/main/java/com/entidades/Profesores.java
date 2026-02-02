73916468de6c5ad6bafe3dfae06b7da30d1e71d8
AlejandroH91 <hernadez.razo.alejandro@iescamas.es>
2026-02-02 16:53:30 +0000

    Añadido el paquete entidades y entidad profesor

---
+++ src/main/java/com/entidades/Profesores.java
@@ -0,0 +1,82 @@
+package com.entidades;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+@Entity
+public class Profesores {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)//El generated Value para hacerlo autoincremental.
+	private int id;
+	@NotBlank(message = "No puedes dejar los campos en blanco")
+	@Size(min = 6, max = 20)
+	@Pattern(regexp = "^[A-Za-zÁÉÍÓÚáéíóúÑñ ]+$",
+    message = "El nombre solo puede contener letras")
+	private String nombre, apellido1, apellido2;
+	@NotBlank(message = "No puedes dejar los campos en blanco")
+	@Size(min = 6, max = 40, message="Longitud minima de 6")
+	private String pass;
+	@NotBlank(message = "El email no puede estar vacío")
+	@Email(message = "Debe introducir un email válido Ej: juanita23@gmail.com")
+	private String email;
+	
+	public Profesores (String nombre,String apellido1,String apellido2,String email,String pass) {
+		this.nombre=nombre;
+		this.apellido1=apellido1;
+		this.apellido2=apellido2;
+		this.email=email;
+		this.pass=pass;
+	}
+
+	public Profesores () {
+		
+	}
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido1() {
+		return apellido1;
+	}
+
+	public void setApellido1(String apellido1) {
+		this.apellido1 = apellido1;
+	}
+
+	public String getApellido2() {
+		return apellido2;
+	}
+
+	public void setApellido2(String apellido2) {
+		this.apellido2 = apellido2;
+	}
+
+	public String getPass() {
+		return pass;
+	}
+
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+
+}
